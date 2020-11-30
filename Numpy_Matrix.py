14bf1aa680ab33aad947b600e4db44711975d66d
tandat-1305 <72129432+tandat-1305@users.noreply.github.com>
2020-11-30 21:45:47 +0700

    Add files via upload

---
+++ Numpy_Matrix.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 21:32:34 2020
+
+@author: Administrator
+"""
+#Numpy
+import numpy as np
+a = np.array([[1, 2],
+             [3, 4]])
+b = np.array([[10, 20,],
+              [40, 50,]])
+c = np.array([[2,4,6],[1,3,5]])
+print("Tổng hai ma trận: ")
+print(a+b)
+print("Tích a và b: ")
+print(a.dot(b))
+print("Ma trận chuyển vị: ")
+print(np.transpose(c))
+#Pandas
+import pandas as pd
+
+
