d70adb416629955eaa1cd8266d1fe50dacd12c0a
tandat-1305 <72129432+tandat-1305@users.noreply.github.com>
2021-04-26 11:02:13 +0700

    Add files via upload

---
+++ prod_funtion.R
@@ -0,0 +1,38 @@
+#Câu 1
+
+answer <- 0
+for (j in 3:5){ answer <- j+answer }
+answer
+answer<- 10
+for (j in 3:5){ answer <- j+answer }
+answer
+answer <- 10
+for (j in 3:5){ answer <- j*answer }
+answer
+
+#Câu 2
+answer <- prod(3:5, start =10)
+answer
+
+#Câu 3
+sum(1:100)
+x <- 0
+for (i in 1:100)
+  x <- x+i
+x
+
+#Câu 4
+anwer <- 1
+for (i in 1:50){answer <- answer*i}
+answer
+
+answer <- (1:50)
+prod(answer)
+
+#Câu 5
+r <- (3:20)
+V <- 4*pi*r/3
+table <- data.frame(r=r,V=V)
+table
+
+#Câu 6
